e1ade229c9ea70f4757303cb3e107987b2d411a5
Michael Pail <143653594+Bucket1616@users.noreply.github.com>
2026-03-17 16:27:27 -0400

    Update ToDoList.hooks.php
    Add hooks for second style

--- includes/ToDoList.hooks.php
+++ includes/ToDoList.hooks.php
@@ -15,14 +15,26 @@ class ToDoListHooks
 	public static function onParserSetup(Parser $parser)
 	{
 		$parser->setHook('todo', 'ToDoListHooks::processToDoListTag');
+		$parser->setHook('todocircle', 'ToDoListHooks::processToDoListTagCircle');
 		return true;
 	}
 
+	Handler for square <todo>
+	public static function processToDoListTag($input, array $args, Parser $parser, PPFrame $frame)
+	{
+		return self::renderInternal($args, $parser, 'todo');
+	}
 
-	/**
+	Handler for circle <todocircle>
+	public static function processToDoListTagCircle($input, array $args, Parser $parser, PPFrame $frame)
+	{
+		return self::renderInternal($args, $parser, 'todo2');
+	}
+
+	 /**
 	 * Implementation of the '<todo>' tag processing
 	 */
-	public static function processToDoListTag($input, array $args, Parser $parser, PPFrame $frame)
+	public static function renderInternal(array $args, Parser $parser, $tagName)
 	{
 		$out = $parser->getOutput();
 		OutputPage::setupOOUI();
@@ -34,12 +46,17 @@ class ToDoListHooks
 			$isDone =  filter_var($args['done'], FILTER_VALIDATE_BOOLEAN);
 		}
 
-		//return '<input type="checkbox" class="todo-checkbox oo-ui-inputWidget-input"' . ( $isDone ? ' checked' : '' ) . '>';
+		// Determine specific classes
+		$extraClass = ($tagName === 'todo2') ? 'todo-circle' : 'todo-square';
 
+		// We add the 'todo-checkbox' class for the JS to find it,
+		// and 'todo-circle'/'todo-square' for styling and logic.
 		$checkboxControl = new OOUI\CheckboxInputWidget([
 			'selected' => $isDone,
-			'classes' => ['todo-checkbox']
+			'classes' => ['todo-checkbox', $extraClass]
 		]);
+
+		// Return the widget
 		return [$checkboxControl->toString(), 'markerType' => 'nowiki'];
 	}
 }
